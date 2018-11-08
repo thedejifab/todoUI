6148b37d667994e7b21c18d4f1a6074ecdb23784
Fabusuyi012 <fabgraphicsng@gmail.com>
2018-11-08 04:58:54 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(new MyApp());
 
@@ -21,6 +22,22 @@ class TodoList extends StatefulWidget {
 
 class TodoListState extends State<TodoList> {
   List<String> _todoItems = [];
+  TimeOfDay _time = new TimeOfDay.now();
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: _time,
+    );
+
+    if (picked != null && picked != _time) {
+      print("Date selected: ${_time.toString}");
+      setState(() {
+        _time = picked;
+        print(_time);
+      });
+    }
+  }
 
   void _addTodoItem(String task) {
     setState(() {
@@ -72,213 +89,231 @@ class TodoListState extends State<TodoList> {
   Widget _pushAddTodoScreen() {
     Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
       return new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Add a new task"),
-            actions: <Widget>[
-              IconButton(
-                icon: new Icon(
-                  Icons.tune,
-                  semanticLabel: "reset",
-                ),
-                onPressed: () {
-                  print("reset button pressed");
-                },
-              )
-            ],
-          ),
-          body: new Container(
-            margin: const EdgeInsets.only(left: 30.0, right: 30.0),
-            child: new Column(
-              children: <Widget>[
-                new SizedBox(height: 50.0),
-                new Center(
-                  child: new Container(
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: new BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
-                    child: Icon(
-                      Icons.work,
-                      size: 50.0,
-                    ),
+        appBar: new AppBar(
+          title: new Text("Add a new task"),
+          actions: <Widget>[
+            IconButton(
+              icon: new Icon(
+                Icons.tune,
+                semanticLabel: "reset",
+              ),
+              onPressed: () {
+                print("reset button pressed");
+              },
+            )
+          ],
+        ),
+        body: new SingleChildScrollView(
+            child: new Container(
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+          child: new Column(
+            children: <Widget>[              
+              new SizedBox(height: 50.0),
+              new Center(                
+                child: new Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                  ),                  
+                  child: Icon(                    
+                    Icons.work,
+                    size: 50.0,
                   ),
                 ),
-                new SizedBox(height: 12.0),
-                new Container(
-                  width: double.infinity,
-                  child: DropdownButton<String>(
-                      value: "Business",
-                      items: <String>[
-                        "Business",
-                        "Personal",
-                        "Family",
-                        "Uncategorized"
-                      ].map((String value) {
-                        return new DropdownMenuItem<String>(
-                          value: value,
-                          child: new Text(
-                            value,
-                            style: TextStyle(fontSize: 15.0),
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (value) {
-                        setState() {}
-                      },
-                      isExpanded: true,
-                      hint: new Text("Select Task Type")),
-                ),
-                new SizedBox(height: 12.0),
-                new TextField(
-                  autofocus: true,
-                  onSubmitted: (task) {
-                    _addTodoItem(task);
-                    Navigator.pop(context);
-                  },
-                  decoration: new InputDecoration(
-                      // hintText: "Task",
-                      labelText: "Task"),
-                ),
-                new SizedBox(height: 12.0),
-                new TextField(
-                  decoration: new InputDecoration(
-                      // hintText: "Place",
-                      labelText: "Place"),
-                ),
-                new SizedBox(height: 12.0),
-                new TextField(
-                  decoration: new InputDecoration(
-                      // hintText: "Time",
-                      labelText: "Time"),
-                ),
-                new SizedBox(height: 30.0),
-                new ButtonTheme(
-                    minWidth: double.infinity,
-                    child: new RaisedButton(
+              ),
+              new SizedBox(height: 12.0),
+              new Container(
+                width: double.infinity,
+                child: DropdownButton<String>(
+                    value: "Business",
+                    items: <String>[
+                      "Business",
+                      "Personal",
+                    ].map((String value) {
+                      return new DropdownMenuItem<String>(
+                        value: value,
                         child: new Text(
-                          "ADD NEW TODO",
-                          style: new TextStyle(color: Colors.white),
+                          value,
+                          style: TextStyle(fontSize: 15.0),
                         ),
-                        elevation: 4.0,
-                        padding: EdgeInsets.all(15.0),
-                        color: Colors.blue,
-                        splashColor: Colors.blueGrey,
-                        onPressed: () {}))
-              ],
-            ),
-          ));
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState() {}
+                    },
+                    isExpanded: true,
+                    hint: new Text("Select Task Type")),
+              ),
+              new SizedBox(height: 12.0),
+              new TextField(
+                autofocus: true,
+                onSubmitted: (task) {
+                  _addTodoItem(task);
+                  Navigator.pop(context);
+                },
+                decoration: new InputDecoration(
+                    // hintText: "Task",
+                    labelText: "Task"),
+              ),
+              new SizedBox(height: 12.0),
+              new TextField(
+                decoration: new InputDecoration(
+                    // hintText: "Place",
+                    labelText: "Place"),
+              ),
+              new SizedBox(height: 12.0),
+              new GestureDetector(
+                onTap: (){
+                  print("OnTap called for textfield");
+                },
+              child: new TextField(
+                // enabled: false,                
+                decoration: new InputDecoration(
+                    // hintText: "Time",
+                    labelText: "Time"),
+              ),
+              ),
+              new SizedBox(height: 30.0),
+              new ButtonTheme(
+                  minWidth: double.infinity,
+                  child: new RaisedButton(
+                      child: new Text(
+                        "ADD NEW TODO",
+                        style: new TextStyle(color: Colors.white),
+                      ),
+                      elevation: 4.0,
+                      padding: EdgeInsets.all(15.0),
+                      color: Colors.blue,
+                      splashColor: Colors.blueGrey,
+                      onPressed: () {}))
+            ],
+          ),
+        )),
+      );
     }));
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: false,
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: new Container(
-                      padding: EdgeInsets.only(bottom: 30.0, right: 140.0),
-                      // alignment: Alignment.center,
-                      child: new Opacity(
-                        opacity: 0.95,
-                        child: new Text("Your \nTodos",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25.0,
-                                fontWeight: FontWeight.w400)),
-                      )),
-                  background: new Image.asset(
-                    'assets/background.jpg',
-                    fit: BoxFit.cover,
-                  )),
-                  bottom: new TabBar(
-                    tabs: [
-                      new Tab(
-                        new Text("Sep 5, 2018"),
-                      ),
-                      new Tab(
-                        new Text("65% done"),
-                      ),
-                    ],
-                  ),
-              // leading: new Icon(Icons.),
-              leading: new Icon(Icons.menu),
-              actions: <Widget>[              
-                new SingleChildScrollView(                
-                  // padding: EdgeInsets.only(top: 30.0),                  
-                  child: new Container(
-                      alignment: Alignment.bottomCenter,
-                      child: new Column(
-                        children: <Widget>[
-                          new Container(
-                            child: new Row(
-                              children: <Widget>[
-                                new Column(
-                                  children: <Widget>[
-                                    new Text(
-                                      "24",
-                                      style: TextStyle(fontSize: 25.0),
-                                    ),
-                                    new Text(
-                                      "Personal",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.grey),
-                                    )
-                                  ],
-                                ),
-                                new SizedBox(
-                                  width: 15.0,
-                                ),
-                                new Column(
-                                  children: <Widget>[
-                                    new Text(
-                                      "15",
-                                      style: TextStyle(fontSize: 25.0),
-                                    ),
-                                    new Text(
-                                      "Business",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.grey),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: false,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: new Container(
+                        padding: EdgeInsets.only(bottom: 30.0, right: 140.0),
+                        // alignment: Alignment.center,
+                        child: new Opacity(
+                          opacity: 0.95,
+                          child: new Text("Your \nTodos",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.w400)),
+                        )),
+                    background: new Image.asset(
+                      'assets/background.jpg',
+                      fit: BoxFit.cover,
+                    )),
+                bottom: TabBar(
+                  labelColor: Colors.black87,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: [
+                    new Container(
+                      height: 100.0,
+                      alignment: Alignment.bottomLeft,
+                      child: new Tab(
+                        child: new Text(
+                          "Nov 6, 2018",
+                          style: TextStyle(
+                            color: Colors.white,
                           ),
-                          new Container(
-                            child: new Text("65% done"),
-                          )
-                        ],
-                      )),
-                )
-              ],
-            ),
-            new SliverPadding(
-              padding: new EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
-              sliver: new SliverList(
-                delegate: new SliverChildListDelegate([
-                  new Text(
-                    "INBOX",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.bold),
-                  ),
-                ]),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      // alignment: Alignment.bottomCenter,
+                      child: new Tab(
+                        child: new Container(
+                          // padding: EdgeInsets.only(top: 30.0),
+                          child: new Container(
+                              child: new Column(
+                            children: <Widget>[
+                              new Container(
+                                child: new Row(
+                                  children: <Widget>[
+                                    new Column(
+                                      children: <Widget>[
+                                        new Text(
+                                          "24",
+                                          style: TextStyle(fontSize: 25.0),
+                                        ),
+                                        new Text(
+                                          "Personal",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+                                    new SizedBox(
+                                      width: 15.0,
+                                    ),
+                                    new Column(
+                                      children: <Widget>[
+                                        new Text(
+                                          "15",
+                                          style: TextStyle(fontSize: 25.0),
+                                        ),
+                                        new Text(
+                                          "Business",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                // leading: new Icon(Icons.),
+                leading: new Icon(Icons.menu),
+                actions: <Widget>[],
               ),
-            ),
-          ];
-        },
-        body: _buildTodoList(),
+              new SliverPadding(
+                padding:
+                    new EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                sliver: new SliverList(
+                  delegate: new SliverChildListDelegate([
+                    new Text(
+                      "INBOX",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.bold),
+                    ),
+                  ]),
+                ),
+              ),
+            ];
+          },
+          body: _buildTodoList(),
+        ),
       ),
       floatingActionButton: new FloatingActionButton(
           onPressed: _pushAddTodoScreen,
